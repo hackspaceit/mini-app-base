94a9a4c92d8960b13f1a3d5a167b39954f792378
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-10 03:25:22 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,15 @@ import { useEffect, useMemo, useState, useCallback } from "react";
 import { Button } from "./components/DemoComponents";
 import { Icon } from "./components/DemoComponents";
 
-
+import { NFTMintCard } from '@coinbase/onchainkit/nft';
+import { NFTMedia } from '@coinbase/onchainkit/nft/view'; 
+import {
+  NFTCreator, 
+  NFTCollectionTitle, 
+  NFTQuantitySelector, 
+  NFTAssetCost, 
+  NFTMintButton, 
+} from '@coinbase/onchainkit/nft/mint'; 
 
 export default function App() {
   const { setFrameReady, isFrameReady, context } = useMiniKit();
@@ -95,7 +103,17 @@ export default function App() {
           </div>
           <div>{saveFrameButton}</div>
         </header>
-        
+<NFTMintCard
+  contractAddress='0xed2f34043387783b2727ff2799a46ce3ae1a34d2'
+  tokenId='2'
+>
+  <NFTCreator />
+  <NFTMedia />
+  <NFTCollectionTitle />
+  <NFTQuantitySelector />
+  <NFTAssetCost />
+  <NFTMintButton />
+</NFTMintCard>
         <footer className="mt-2 pt-4 flex justify-center">
           <Button
             variant="ghost"
