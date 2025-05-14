998e9c29a5514530cc47ff1869ea59698e1ba32b
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-15 03:33:16 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -42,6 +42,7 @@ export default function App() {
     setFrameAdded(Boolean(frameAdded));
   }, [addFrame]);
 
+
   const saveFrameButton = useMemo(() => {
     if (context && !context.client.added) {
       return (
@@ -93,7 +94,6 @@ export default function App() {
           </div>
           <div>{saveFrameButton}</div>
 
-         
         </header>
  
         <main className="flex-1">
