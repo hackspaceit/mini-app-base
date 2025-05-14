bdf534d6a006331c871c1ddd836cfc5278fd183e
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-15 03:23:49 +0700

    update

--- app/components/MintComponent.tsx
+++ app/components/MintComponent.tsx
@@ -12,7 +12,7 @@ import {
   
 > 
   
-    <NFTMintButton disabled={false}/>
+    <NFTMintButton />
 </NFTMintCard>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ import { useEffect, useMemo, useState, useCallback } from "react";
 import { Button } from "./components/DemoComponents";
 import { Icon } from "./components/DemoComponents";
 import MintComponent from './components/MintComponent';
-import { buyCoin } from "./components/tradeCoinCall";
+
 
 export default function App() {
   const { setFrameReady, isFrameReady, context } = useMiniKit();
@@ -100,8 +100,7 @@ export default function App() {
     
        
           <MintComponent />
-      <buyCoin />
-      
+     
     
 
 
