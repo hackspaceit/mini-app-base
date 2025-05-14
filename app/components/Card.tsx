f01cc821afb4c4a48d5b5873d2e2f8e40ec415e9
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-15 06:46:16 +0700

    update

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function BasicExample() {
   return (
     <Card style={{ width: '18rem' }}>
