a510214b39b73ba5f686854b7b10d687f67d60e0
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-14 15:44:17 +0700

    update

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { type ReactNode } from "react";
-import { monadTestnet } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 export function Providers(props: { children: ReactNode }) {
   return (
     <MiniKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={monadTestnet}
+      chain={base}
       config={{
         appearance: {
           mode: "auto",
