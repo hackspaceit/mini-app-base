cc489ef7197957b86b14d794aba5fbfa6934dac6
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-15 07:06:22 +0700

    update

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -7,11 +7,10 @@ function BasicExample() {
     <Card style={{ width: '26rem' }}>
       <Card.Img variant="top" src="/run1.gif" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Run on Base</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+          Run on Base is a thrilling mini-app game on Farcaster where users compete to earn points and climb the leaderboard. The higher you rank, the more rewards you unlock. Points can be redeemed for crypto assets or future airdrop programs. Play now, rise to the top, and turn your skill into real value!
+       <br></br> Mint early to join! </Card.Text>
         <MintComponent />
         
       </Card.Body>
