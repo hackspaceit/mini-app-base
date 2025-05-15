729b0ceabbb9f5c008b24a1eef8d893b6c93a935
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-15 09:01:38 +0700

    update

--- app/components/MintComponent.tsx
+++ app/components/MintComponent.tsx
@@ -9,7 +9,7 @@ import {
  export default function MintComponent() {
   return (
 <NFTMintCard 
-  contractAddress='0x5d3fa8fc8fcdcd61083940e27977ed64261be53f' 
+  contractAddress='0x363b1a86BB37fc1A1F87c1791119dbe18e9B4aF2' 
   
 > 
   
