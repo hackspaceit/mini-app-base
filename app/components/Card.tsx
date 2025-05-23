fa427f5b9a9f90776cd6ab6174cc5aa4a0ebaab6
hackspaceit <iniakunbisnisku@gmail.com>
2025-05-24 00:31:36 +0700

    update

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,7 +12,7 @@ function BasicExample() {
         <Card.Text>
           Run on Base is a thrilling mini-app game on Farcaster where users compete to earn points and climb the leaderboard. The higher you rank, the more rewards you unlock. Points can be redeemed for crypto assets or future airdrop programs. Play now, rise to the top, and turn your skill into real value!
        <br></br> <b><center>Mint your early PASS! </center></b></Card.Text>
-        <MintComponent /> 
+        <MintComponent /> <br></br>
           <Button variant="primary" href="https://art.fun/coin/0x43ea68aead83b0f33971fe8cb3e629c6d41803d0">Buy $RUN</Button>
       </Card.Body>
     </Card>
